b595f6acb65fbcafff899aa2f99211f4147712e0
qvoste <qvoste@gmail.com>
2025-11-13 21:02:14 +0700

    Easy - 27. Remove Element

---
+++ src/main/java/org/example/task_29/Solution.java
@@ -0,0 +1,22 @@
+package org.example.task_29;
+
+class Solution {
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return(index);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,2,3};
+        int val = 3;
+
+        System.out.println(removeElement(nums, val));
+    }
+}

8ebe0102abaef435d5749fa168a27f1542f78c33
qvoste <qvoste@gmail.com>
2025-11-22 21:51:26 +0700

    Medium - 45. Jump Game II

---
+++ src/main/java/org/example/task_45/Solution.java
@@ -0,0 +1,32 @@
+package org.example.task_45;
+
+class Solution {
+    public static int jump(int[] nums) {
+        int n = nums.length;
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        if (n <= 1) return 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+
+                if (currentEnd >= n - 1) {
+                    return jumps;
+                }
+            }
+        }
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+
+        System.out.println(jump(nums));
+    }
+}

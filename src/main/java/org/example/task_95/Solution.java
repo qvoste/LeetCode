1c77708651e0adf320700c3412b89dc6d0ec264e
qvoste <qvoste@gmail.com>
2025-12-28 23:29:28 +0700

    Medium - 73. Set Matrix Zeroes

---
+++ src/main/java/org/example/task_95/Solution.java
@@ -0,0 +1,53 @@
+package org.example.task_95;
+
+public class Solution {
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] r : matrix) {
+            for (int val : r) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0, 1, 2, 0},
+                {3, 4, 5, 2},
+                {1, 3, 1, 5}
+        };
+
+        System.out.println("До:");
+        printMatrix(matrix);
+
+        setZeroes(matrix);
+
+        System.out.println("После:");
+        printMatrix(matrix);
+    }
+}

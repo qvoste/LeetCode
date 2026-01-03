91ad8251e78261a5475d0d51cd8d2e4516fee9e4
qvoste <qvoste@gmail.com>
2026-01-03 22:52:27 +0700

    Easy - 944. Delete Columns to Make Sorted

---
+++ src/main/java/org/example/task_96/Solution.java
@@ -0,0 +1,24 @@
+package org.example.task_96;
+
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int res = 0;
+        for (int j = 0; j < strs[0].length(); j++)
+            res += isUnsorted(strs, j);
+        return res;
+    }
+
+    private int isUnsorted(String[] strs, int j) {
+        for (int i = 1; i < strs.length; i++)
+            if (strs[i].charAt(j) < strs[i - 1].charAt(j))
+                return 1;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"cba","daf","ghi"};
+        Solution solution = new Solution();
+
+        System.out.println(solution.minDeletionSize(strs));
+    }
+}
